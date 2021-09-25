a9f26779b929fb19279d729c8a35080906fff786
Toshiaki Maki <tmaki@vmware.com>
2021-09-25 22:50:04 +0900

    Support native build

--- tsunagu-client/src/main/java/am/ik/tsunagu/TsunaguClientApplication.java
+++ tsunagu-client/src/main/java/am/ik/tsunagu/TsunaguClientApplication.java
@@ -3,9 +3,20 @@ package am.ik.tsunagu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.http.HttpHeaders;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.TypeHint;
 
 @SpringBootApplication
 @EnableConfigurationProperties(TsunaguProps.class)
+@NativeHint(options = {
+		"--enable-http",
+		"--enable-https",
+		"--enable-all-security-services",
+		"-Dfile.encoding=UTF-8"
+}, types = {
+		@TypeHint(types = { HttpHeaders.class, HttpRequestMetadata.class, HttpResponseMetadata.class })
+})
 public class TsunaguClientApplication {
 
 	public static void main(String[] args) {
--- tsunagu-server/src/main/java/am/ik/tsunagu/TsunaguServerApplication.java
+++ tsunagu-server/src/main/java/am/ik/tsunagu/TsunaguServerApplication.java
@@ -4,8 +4,19 @@ import reactor.core.publisher.Hooks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.TypeHint;
 
 @SpringBootApplication
+@NativeHint(options = {
+		"--enable-http",
+		"--enable-https",
+		"--enable-all-security-services",
+		"-Dfile.encoding=UTF-8"
+}, types = {
+		@TypeHint(types = { HttpHeaders.class, HttpRequestMetadata.class, HttpResponseMetadata.class })
+})
 public class TsunaguServerApplication {
 
 	public static void main(String[] args) {
