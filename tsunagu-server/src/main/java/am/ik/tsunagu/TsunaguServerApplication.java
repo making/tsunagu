88c06f679ec46ffdd1841437f44ee90691270cee
Toshiaki Maki <tmaki@vmware.com>
2021-09-26 19:51:48 +0900

    Add Sleuth

--- tsunagu-server/src/main/java/am/ik/tsunagu/TsunaguServerApplication.java
+++ tsunagu-server/src/main/java/am/ik/tsunagu/TsunaguServerApplication.java
@@ -11,14 +11,23 @@ import org.springframework.nativex.hint.TypeHint;
 
 @SpringBootApplication
 @EnableConfigurationProperties(TsunaguProps.class)
-@NativeHint(options = {
-		"--enable-http",
-		"--enable-https",
-		"--enable-all-security-services",
-		"-Dfile.encoding=UTF-8"
-}, types = {
-		@TypeHint(types = { HttpHeaders.class, HttpRequestMetadata.class, HttpResponseMetadata.class })
-})
+@NativeHint(
+		options = {
+				"--enable-http",
+				"--enable-https",
+				"--enable-all-security-services",
+				"-Dfile.encoding=UTF-8"
+		},
+		types = {
+				@TypeHint(
+						types = { HttpHeaders.class, HttpRequestMetadata.class, HttpResponseMetadata.class },
+						typeNames = {
+								"org.springframework.cloud.sleuth.autoconfig.zipkin2.ZipkinActiveMqSenderConfiguration",
+								"org.springframework.cloud.sleuth.autoconfig.zipkin2.ZipkinRabbitSenderConfiguration",
+								"org.springframework.cloud.sleuth.autoconfig.zipkin2.ZipkinKafkaSenderConfiguration"
+						}
+				)
+		})
 public class TsunaguServerApplication {
 
 	public static void main(String[] args) {
